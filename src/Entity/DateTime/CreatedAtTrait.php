89e189a082d24def5d03ba4aad4cea8fb016cf7b
Jason Raimondi <jason@raimondi.us>
2017-05-15 23:13:47 -0700

    Adding updated and created at traits.

---
+++ src/Entity/DateTime/CreatedAtTrait.php
@@ -0,0 +1,32 @@
+<?php
+namespace Jmondi\Gut\Entity\DateTime;
+
+use DateTime;
+use DateTimeZone;
+
+trait CreatedAtTrait
+{
+    /** @var int */
+    protected $createdAt;
+
+    /**
+     * @param DateTime $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt = null)
+    {
+        if ($createdAt === null) {
+            $createdAt = new DateTime('now', new DateTimeZone('UTC'));
+        }
+        $this->createdAt = $createdAt->getTimestamp();
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        $createdAt = new DateTime();
+        $createdAt->setTimestamp($this->createdAt);
+        return $createdAt;
+    }
+}
