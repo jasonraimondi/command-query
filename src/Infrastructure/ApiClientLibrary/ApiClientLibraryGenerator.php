f9b3c1e4624e2b8d77f4a3a7d9ba47e6b117a068
Jason Raimondi <jason@raimondi.us>
2017-05-18 23:46:38 -0700

    Removing junk.

--- src/Infrastructure/ApiClientLibrary/ApiClientLibraryGenerator.php
+++ src/Infrastructure/ApiClientLibrary/ApiClientLibraryGenerator.php
@@ -5,14 +5,6 @@ use Jmondi\Gut\Infrastructure\ApiClientLibrary\Clients\TypescriptClient;
 
 class ApiClientLibraryGenerator
 {
-    /** @var string */
-    private $clientLibraryTemplatePath;
-
-    public function __construct()
-    {
-        $this->clientLibraryTemplatePath = realpath(__DIR__ . '/../../../') . '/templates/api-client-libraries';
-    }
-
     public function execute()
     {
         $typescriptClient = TypescriptClient::createNewClient();
