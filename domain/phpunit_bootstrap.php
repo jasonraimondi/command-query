691f0dcf2d7406359bebc773823caccece4f2586
Jason Raimondi <jason@raimondi.us>
2017-06-18 23:00:42 -0700

    Add phpunit.xml and phpunit bootstrap.

---
+++ domain/phpunit_bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+namespace Jmondi\Gut\Infrastructure\Lib;
+
+error_reporting(E_ALL);
+date_default_timezone_set('UTC');
+ini_set('memory_limit', '256M');
+
+function is_uploaded_file($fileName)
+{
+    return file_exists($fileName);
+}
