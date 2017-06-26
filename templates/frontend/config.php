a528b60c89fd78ef843fd0b105e533cea49e50d2
Jason Raimondi <jason@raimondi.us>
2017-06-25 18:45:42 -0700

    Remove old docker-compose references. Update config for frontend project.

--- templates/frontend/config.php
+++ templates/frontend/config.php
@@ -3,7 +3,7 @@ use Jmondi\Gut\Infrastructure\Template\Twig\TwigThemeConfig;
 
 return new TwigThemeConfig(
     'frontend',
-    'Angular',
+    'Frontend Project',
     'The default admin theme for 2016',
     __DIR__
 );
